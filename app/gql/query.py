9e91116c472a9c67e1b1042962e216f295dfb10e
ari oren <arioren100@gmail.com>
2024-10-31 11:27:24 +0200

    second query mission by range of dates

--- app/gql/query.py
+++ app/gql/query.py
@@ -1,4 +1,4 @@
-from graphene import ObjectType, Field, Int, List
+from graphene import ObjectType, Field, Int, List, Date
 
 from app.db.database import session_maker
 from app.db.model import Mission
@@ -17,9 +17,21 @@ class Query(ObjectType):
                 .first()
             )
 
+    # .2 מציאת משימות לפי טווח תאריכים: מקבלים רשימה של פרטי כל המשימות
+    # הרלוונטיות
+    missionByDate = List(MissionType, start_date=Date(), end_date=Date())
+    @staticmethod
+    def resolve_missionByDate(root, info, start_date, end_date):
+        with session_maker() as session:
+            return(
+                session.query(Mission)
+                .filter(Mission.mission_date >= start_date,
+                        Mission.mission_date <= end_date)
+                .all()
+            )
 
-# .2 מציאת משימות לפי טווח תאריכים: מקבלים רשימה של פרטי כל המשימות
-# הרלוונטיות
+
+
 # .3 מציאת משימה לפי מדינה שלקחו בו חלק: מקבלים רשימה של פרטי כל
 # המשימות המתאימות
 # .4 מציאת משימה לפי תעשיית מטרה )Industry Target): מקבלים רשימה של
